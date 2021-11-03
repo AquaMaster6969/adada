b532c290bc89783972e65d25dc016ecd32045b8f
AquaMaster6969 <73968996+AquaMaster6969@users.noreply.github.com>
2021-11-02 20:13:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,11 @@ rightWristX = 0;
 leftWrsitX = 0;
 
 function setup() {
-    video = createCapture(VIDEO);
-    video.size(550, 500);
-    video.hide();
-
     canvas = createCanvas(550, 500);
     canvas.position(560, 150);
+
+    video = createCapture(VIDEO);
+    video.size(550, 500);
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
@@ -43,6 +42,6 @@ function draw() {
     fill('red');
     stroke('red');
     textSize(difference);
-    text('Shehzadi', noseX, noseY);
+    text('Emerson', noseX, noseY);
 }
 
